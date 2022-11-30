8167d8aa4ccd452761a2b3a9722dbfb4dfb522b3
scifaclint <scifaclint@gmail.com>
2022-11-30 10:27:46 +0000

    genging

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+# YOUR CODE HERE
+if number > 0 :
+    print('is positive')
+if number == 0 :
+    print('is zero')
+if number < 0 :
+    print('is negative')
+   
