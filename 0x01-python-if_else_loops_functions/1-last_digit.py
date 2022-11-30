f64f6171d8f84a13faf9f1ec8b2bf2a4eb29f197
scifaclint <scifaclint@gmail.com>
2022-11-30 12:03:25 +0000

    genging

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,16 +4,34 @@ import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
 getstra = str(number)
-conumber = int(getstra[-1])
-if conumber > 5:
-    print("Last digit of", number, "is", conumber, "and is greater than 5")
-if conumber == 0:
-    print("Last digit of", number, "is", conumber, "and is 0")
-if conumber < 6 and conumber != 0:
-    print(
-        "Last digit of",
-        number,
-        "is",
-        conumber,
-        "and is less than 6 and not 0"
-    )
+number_positive = None
+numbernegative = None
+if number > 0:
+    number_positive = int(getstra[-1])
+    if number_positive > 5:
+        print("Last digit of", number, "is", number_positive, "and is greater than 5")
+    if number_positive == 0:
+        print("Last digit of", number, "is", number_positive, "and is 0")
+    if number_positive < 6 and number_positive != 0:
+        print(
+            "Last digit of",
+            number,
+            "is",
+            number_positive,
+            "and is less than 6 and not 0",
+        )
+else:
+    numbernegative = -int(getstra[-1])
+    print(numbernegative)
+    if numbernegative > 5:
+        print("Last digit of", number, "is", numbernegative, "and is less than 5")
+    if numbernegative == 0:
+        print("Last digit of", number, "is", numbernegative, "and is 0")
+    if numbernegative < 6 and numbernegative != 0:
+        print(
+            "Last digit of",
+            number,
+            "is",
+            numbernegative,
+            "and is less than 6 but not 0"
+        )
