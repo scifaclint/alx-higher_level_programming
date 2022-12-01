6c993a03c32be6847339441666309ff5cf03259f
scifaclint <scifaclint@gmail.com>
2022-12-01 23:30:31 +0000

    genging lol

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import sys
+
+
+def getargv(list):
+    list.remove(list[0])
+    if len(list) == 0:
+        print("0 arguments.")
+    else:
+        if len(list) == 1:
+            print("{} argument:".format(len(list)))
+            print("1: {}".format(list[0]))
+        if len(list) > 1:
+            print("{} arguments:".format(len(list)))
+            a = 0
+            b = 1
+            while a < len(list):
+                print("{}: {}".format(b, list[a]))
+                b += 1
+                a += 1
+
+
+getargv(sys.argv)
