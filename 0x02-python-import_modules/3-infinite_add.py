a521fcc70e95c4354bf25fed01f0c06dcb130d80
scifaclint <scifaclint@gmail.com>
2022-12-02 00:42:28 +0000

    l

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,19 +4,15 @@
 if __name__ == "__main__":
     """Print the number of and list of arguments."""
     import sys
-
-    def getargv(list):
-        list.remove(list[0])
-        getint = int(list[0])
-        print(list)
-        if len(list) == 0:
-            print(len(list))
-        else:
-            a = 1
-            while a < len(list):
-                getint += int(list[a])
-                a += 1
-        print(getint)
-
-
-getargv(sys.argv)
+    arguments = sys.argv
+    arguments.remove(arguments[0])
+    total = 0
+    for a in range(len(arguments)):
+        total += int(arguments[a])
+    print(total)
+        
+        
+        
+        
+        
+  
