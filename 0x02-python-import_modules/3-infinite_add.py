4ad44aa9cbe15ff8afe8183aa4ef72529fee422e
scifaclint <scifaclint@gmail.com>
2022-12-02 00:46:28 +0000

    suii

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,15 +4,10 @@
 if __name__ == "__main__":
     """Print the number of and list of arguments."""
     import sys
+
     arguments = sys.argv
     arguments.remove(arguments[0])
     total = 0
     for a in range(len(arguments)):
         total += int(arguments[a])
     print("{}".format(total))
-        
-        
-        
-        
-        
-  
