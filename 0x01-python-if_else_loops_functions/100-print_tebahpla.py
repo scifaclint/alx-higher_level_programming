cbbf6424859169329e773dd98f671d3ced33d120
scifaclint <scifaclint@gmail.com>
2023-11-02 01:34:29 +0000

    damm

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
+for i in range(26):
 
-
-current_char = 'z'
-
-for i in range(122, 64, -1):
-
-    print(f"{current_char}", end="")
-
-    current_char = current_char.swapcase()
-
-print("Y", end="")
+    print(chr(122 - i).swapcase() if i % 2 == 0 else chr(122 - i), end="")
