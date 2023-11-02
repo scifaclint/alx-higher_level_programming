ba24e447afafd77627b0dc05650bc9c039e431c8
scifaclint <scifaclint@gmail.com>
2023-11-02 20:58:33 +0000

    fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,9 @@ if __name__ == "__main__":
 
     if len(argv) < 1:
         print("{} Arguments.".format(0))
+    elif len(argv) == 1:
+        print("{}: Argument:".format(len(argv)))
+        print("{}: {}".format(len(argv), argv[0]))
     else:
         print("{} Arguments:".format(len(argv)))
         for i, j in enumerate(argv):
