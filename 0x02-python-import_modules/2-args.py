fba5a76fa0694a08bc6b1ea768de7653c129ba1c
scifaclint <scifaclint@gmail.com>
2023-11-02 21:02:22 +0000

    fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,9 @@ if __name__ == "__main__":
     from sys import argv
 
     if len(argv) == 1:
-        print("{} Arguments.".format(0))
+        print("{} Arguments.".format(len(argv)-1))
     elif len(argv) == 2:
-        print("{}: Argument:".format(len(argv)))
+        print("{}: Argument:".format(len(argv)-1))
         print("{}: {}".format(len(argv)-1, argv[1]))
     else:
         print("{} Arguments:".format(len(argv)))
