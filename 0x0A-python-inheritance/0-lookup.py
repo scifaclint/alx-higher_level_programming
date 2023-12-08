41d604651e77787f9e80b5f24d12dbd457f699ae
scifaclint <scifaclint@gmail.com>
2023-12-08 11:13:37 +0000

    genging

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    to print list form of objects
+"""
+
+
+def lookup(obj):
+    """
+    funtion to return list of 
+    any object
+
+    args: object any
+
+    return: list of any object
+
+    """
+    return (dir(obj))
