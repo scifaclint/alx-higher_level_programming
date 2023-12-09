9fa9839dd30a8f61d6804c6db9653789d6fdc35f
scifaclint <scifaclint@gmail.com>
2023-12-09 11:19:14 +0000

    indent fix

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,16 +2,16 @@
 """
     This writes and returns a json
 """
-import json as j
+import json
 
 
 def to_json_string(my_obj):
     """
-    This returns a json representation
+    This returns a json representation.
 
     Args:
-        my_obj (_type_): _description_
+        my_obj (_type_): _description_.
     Returns:
-        Str: Json representation of object 
+        Str: Json representation of object.
     """
-    return j.dumps(my_obj)
+    return json.dumps(my_obj)
