67c659dfc02a3cade9c76726b96a97da78989a8a
scifaclint <scifaclint@gmail.com>
2023-12-09 10:44:18 +0000

    write

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""This scripts writes 
+"""
+This scripts writes 
        to a file .
-    """
+"""
 
 
 def write_file(filename="", text=""):
-    """this function is to write a text
+    """
+    this function is to write a text
 
     Args:
         filename (str, optional): _description_. Defaults to "".
-        text (str, optional): _description_. Defaults to "".
+    Returns:
+        None
     """
     with open(f"{filename}", "w", encoding="utf-8") as file:
         return file.write(text)
-
-
-print(write_file("kofi.txt", "This School is so cool\n"))
