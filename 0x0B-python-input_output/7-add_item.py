ecbffc7b2625e0e25f71d759dca89744a1608aa4
scifaclint <scifaclint@gmail.com>
2023-12-09 14:06:28 +0000

    add and save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,9 +3,24 @@
     This script is used to add all arguments used earlier.
 """
 import sys
+from os import path
 arguments = sys.argv[1:]
+
+FILENAME = "add_item.json"
+# imported functions
 save_to_json_file = __import__(
-    "5-save_to_json_file").save_to_json_file(arguments, "add_item.json")
+    "5-save_to_json_file").save_to_json_file
 
 load_from_json_file = __import__(
-    "6-load_from_json_file").load_from_json_file("add_item.json")
+    "6-load_from_json_file").load_from_json_file
+
+# check if file exist
+if path.exists(FILENAME):
+    # load data from file
+    data = load_from_json_file(FILENAME)
+    for i in arguments:
+        data.append(i)
+        # save data to file
+        save_to_json_file(FILENAME, data)
+else:
+    save_to_json_file(FILENAME, arguments)
