734e9b206ae0cf5ce476b982e74b76742e59f1fc
scifaclint <scifaclint@gmail.com>
2023-12-09 11:00:00 +0000

    indent fix

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
     This writes and appends to existing file
-    """
+"""
 
 
 def append_write(filename="", text=""):
@@ -13,9 +13,6 @@ def append_write(filename="", text=""):
             text (str, optional): _description_. Defaults to "".
         Returns:
             int: number of characters added.
-        """
+    """
     with open(filename, "a", encoding="utf-8") as file:
         return file.write(text)
-
-
-
