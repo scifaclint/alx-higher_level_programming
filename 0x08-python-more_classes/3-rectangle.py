416433144463ade149e7a451436280e08060ece6
scifaclint <scifaclint@gmail.com>
2024-01-05 23:18:21 +0000

    more

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class here """
+
+
+class Rectangle:
+    """A Rectangle class with width and height and more """
+
+    def __init__(self, width=0, height=0):
+        """Initialize  Rectangle instance"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get or set the width of the Rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the Rectangle here"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get or set the height of the Rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the Rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Returns the area of rectangle"""
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        else:
+            return self.__height * self.__width
+
+    def perimeter(self):
+        """Returns the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return ((2 * self.__height) + (2 * self.__width))
+    def __str__(self):
+        """Print the string form of rectangle"""
+        if self.__height == 0 or self.__width == 0 :
+            return ""
+        else:
+            return "##"
+    def __repr__(self):
+        """Return the string representation of"""
+        if self.__height == 0 or self.__width == 0 :
+            return ""
+        else:
+            return "##"
+        
+        
