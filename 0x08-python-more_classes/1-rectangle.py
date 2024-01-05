6fab22926946ee2709ad871903a27bbc9562f31e
scifaclint <scifaclint@gmail.com>
2024-01-05 13:12:49 +0000

    set and get

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -33,8 +33,7 @@ class Rectangle():
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
@@ -53,8 +52,7 @@ class Rectangle():
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
 
 
 
