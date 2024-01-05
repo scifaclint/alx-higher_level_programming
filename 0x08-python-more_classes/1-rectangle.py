c0522d58780ef311075fa9d15a9ee0c438a1b326
scifaclint <scifaclint@gmail.com>
2024-01-05 12:47:03 +0000

    rectangle dr

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" class to define a rectangle class
+"""
+
+
+class Rectangle():
+    """ instances and rest defined here """
+
+    def __init__(self, height=0, width=0):
+        """create private instances
+
+        Args:
+            height (int): must be greater than 0
+            width (int): must be greater than 0
+        """
+        self.height = height
+        self.width = width
+
+    def width(self):
+        """ retrieve width value
+        """
+        return self.width
+
+    def width(self, value):
+        """set value of width 
+
+        Args:
+            value (int): must be 0 or greater
+        """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.width = value
+
+    def height(self):
+        """ retrieve value of height
+        """
+        return self.height
+
+    def height(self, value):
+        """set value of height
+
+        Args:
+            value (int): must be 0 or greater
+        """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.height = value
