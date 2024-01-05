0eae85faf8106bf7d496ca39f3bdfaeb72d79943
scifaclint <scifaclint@gmail.com>
2024-01-05 11:56:06 +0000

    0-rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# class rectangle
+
+class Rectangle():
+    def __init__(self):
+        """initialize a python class
+
+        """
+        pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+# python classes and more into it
