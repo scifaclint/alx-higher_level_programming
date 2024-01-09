8b4c50234c0a930d450cea500aacef6dd0969cb8
scifaclint <scifaclint@gmail.com>
2024-01-09 11:42:49 +0000

    staged output

---
+++ 0x0A-python-inheritance/0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Function to read file output"""
+
+
+def read_file(filename=""):
+    """
+    This reads file and print to stdout
+
+    Args:
+        filename (str, optional): name of the file .
+    Return:
+        None
+    """
+    with open(filename, "r", encoding="UTF8") as filed:
+        output = filed.read()
+        print(output)
