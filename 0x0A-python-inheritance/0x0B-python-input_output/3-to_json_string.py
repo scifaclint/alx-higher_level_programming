a3ba54c76653bc761504b11b6090dd6625e3623c
scifaclint <scifaclint@gmail.com>
2024-01-10 02:21:25 +0000

    input output

---
+++ 0x0A-python-inheritance/0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Function that returns Jason representation of object string"""
+
+
+def to_json_string(my_obj):
+    """
+    Function that takes an object
+
+    Args:
+        my_obj (Str): any object
+    Return:
+        JSON representation of an object (string):
+    """
+    import json
+    
